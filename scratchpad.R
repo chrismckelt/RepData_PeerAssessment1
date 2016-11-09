d00d2876de219daf403934ddc3d90c875ccd267f
Chris McKelt <chris@mckelt.com>
2016-11-09 20:56:48 +0800

    wip

--- scratchpad.R
+++ scratchpad.R
@@ -33,29 +33,62 @@ abline(v = mean.daily.steps, col = "red", lwd = 1)
 abline(v = median.daily.steps, col = "blue", lwd = 1)
 legend(x = "topright", legend = c("mean", "median"), col = c("red", "blue"), bty = "n", lwd = 3)
 
-
 ## What is the average daily activity pattern?
 activity.interval <- activity.data %>% group_by(interval) %>% summarise(mean_steps = mean(steps, na.rm = TRUE))
 
 #Make a time series plot (i.e. type = "l") of the 5-minute interval (x-axis) and the average number of steps taken, averaged across all days (y-axis)
-
 activity.interval.plot <- ggplot(data = activity.interval, mapping = aes(x = interval, y = mean_steps)) +
     geom_line() + scale_x_continuous("Day Interval", breaks = seq(min(activity.interval$interval),
     max(activity.interval$interval), 100)) + scale_y_continuous("Average Number of Steps") +
-    ggtitle("Average Number of Steps Taken by Interval")
+ggtitle("Average Number of Steps Taken by Interval")
 activity.interval.plot
 
 #Which 5-minute interval, on average across all the days in the dataset, contains the maximum number of steps?
 max.steps = activity.interval[activity.interval$mean_steps==which.max(activity.interval$mean_steps)]
- 
+#max.steps:`r max.steps`
 
 ## Imputing missing values
 
 #Calculate and report the total number of missing values in the dataset (i.e. the total number of rows with NAs)
-#missing.days <- is.na(activity.data$steps)
 present.days <- na.omit(activity.data$steps)
 missing.days <- length(activity.data$steps) - length(present.days)
+#strategy for filling in missing data (just replace with mean)
+activity.data$steps[is.na(activity.data$steps)] <- mean.daily.steps
+#Create a new dataset that is equal to the original dataset but with the missing data filled in.
+strategy <- function(x) { if (is.na(x)) mean.daily.steps else x)}
+activity.data.no.na <- activity.data
+for (i in seq(nrow(activity.data.no.na))) {
+    if (is.na(data[i, "steps"])) {
+        data$steps_revised[i] <- data[i, "average_steps"]
+    }
+    else {
+        data$steps_revised[i] <- data[i, "steps"]
+    }
+
+ 
+
+#Make a histogram of the total number of steps taken each day and Calculate and report the mean and median total number of steps taken per day. Do these values differ from the estimates from the first part of the assignment? What is the impact of imputing missing data on the estimates of the total daily number of steps?
+
+#repeat first part of assignment using non NA valued data
+#Calculate the total number of steps taken per day
+total.steps.no.na <- tapply(activity.data.no.na$steps, activity.data.no.na$date, FUN = sum, na.rm = TRUE)
+daily.steps.no.na <- total.steps.no.na %>% group_by(date) %>% summarise(Steps = sum(steps))
+
+names(daily.steps.no.na) <- c("Date", "steps")
+
+#Calculate and report the mean and median of the total number of steps taken per day
+mean.daily.steps.no.na <- mean(total.steps.no.na, na.rm = TRUE)
+median.daily.steps.no.na <- median(total.steps.no.na, na.rm = TRUE)
+
+
+
+hist(daily.steps.no.na$steps, breaks = 11,
+     xlab = "number of steps per day 2",
+     main = "Histogram of total steps per day 2")
+abline(v = daily.steps.no.na, col = "red", lwd = 1)
+abline(v = median.daily.steps.no.na, col = "blue", lwd = 1)
+legend(x = "topright", legend = c("mean", "median"), col = c("red", "blue"), bty = "n", lwd = 3)
+
 
 ## Are there differences in activity patterns between weekdays and weekends?
-
 
